b69d1986f6c56dc7f10eb81a43d26953f97420c4
Jay Bisa <jay.bisa@gmail.com>
2020-11-24 10:55:44 -0500

    Fix resume button on mobile.

--- src/components/Navbar/MenuItems.js
+++ src/components/Navbar/MenuItems.js
@@ -18,5 +18,10 @@ export const MenuItems = [
         title: 'Contact',
         url: '#',
         cName: 'nav-links'
+    },
+    {
+        title: 'Resume',
+        url: '#',
+        cName: 'nav-links-mobile'
     }
 ]
