193e83c38d859926e9d78b61d1831bb37f54aa36
Jay Bisa <jay.bisa@gmail.com>
2020-12-30 16:08:18 -0500

    Added soft skills chart.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+import './Skills.css'
 import CanvasJSReact from '../canvasjs.react';
 
 const Skills = () => {
@@ -8,20 +9,21 @@ const Skills = () => {
         Aos.init({ duration: 2000 });
     }, []);
 
-    const options = {
+    const techSkills = {
         animationEnabled: true,
         animationDuration: 2000,
         theme: "dark2",
         height: 500,
         backgroundColor: "#14213D",
         title:{
-            text: "Proficiency of Skills",
+            text: "Technical Skills",
             fontFamily: 'Titillium Web',
             padding: 20
         },
         axisX: {
             reversed: true,
-            margin: 30
+            margin: 30,
+            interval: 1
         },
         axisY: {
             includeZero: true,
@@ -35,14 +37,58 @@ const Skills = () => {
                 { y:  5, label: "Software Development" },
                 { y:  4, label: "C#" },
                 { y:  4, label: "Java" },
-                { y:  4, label: "Leadership" },
                 { y:  3, label: "PHP" },
+                { y:  1, label: "Go" },
+                { y:  1, label: "Python" },
                 { y:  3, label: "SQL" },
-                { y:  3, label: "Apache Kafka" },                
-                { y:  3, label: "Google BigQuery" },
-                { y:  3, label: "Docker" },
                 { y:  2, label: "ReactJS" },
-                { y:  2, label: "Kubernetes" }
+                { y:  3, label: "Apache Kafka" },  
+                { y:  3, label: "Google BigQuery" },
+                { y:  1, label: "Machine Learning" },
+                { y:  2, label: "Kubernetes" },
+                { y:  2, label: "Docker" },
+                { y:  3, label: "Design Patterns" },
+                { y:  4, label: "Test Automation" }
+            ]
+        }]
+    }
+
+    const softSkills = {
+        animationEnabled: true,
+        animationDuration: 2000,
+        theme: "dark2",
+        height: 500,
+        backgroundColor: "#14213D",
+        title:{
+            text: "Soft Skills",
+            fontFamily: 'Titillium Web',
+            padding: 20
+        },
+        axisX: {
+            reversed: true,
+            margin: 30,
+            labelAutoFit: true,
+            interval: 1
+        },
+        axisY: {
+            includeZero: true,
+            maximum: 5,
+            interval: 1,
+            margin: 30
+        },
+        data: [{
+            type: "bar",
+            dataPoints: [
+                { y:  5, label: "Leadership" },
+                { y:  4, label: "Mentoring" },
+                { y:  5, label: "Teamwork" },
+                { y:  3, label: "Interpersonal Skills" },
+                { y:  3, label: "Communication" },
+                { y:  4, label: "Adaptability" },
+                { y:  4, label: "Problem Solving" },
+                { y:  4, label: "Creativity" },
+                { y:  5, label: "Work Ethic" },
+                { y:  3, label: "Time Management" }
             ]
         }]
     }
@@ -52,13 +98,14 @@ const Skills = () => {
             <h1 className="header" data-aos="fade-up">Skills</h1>
             <ul data-aos="fade-up" className="list">
                 <li>0 - No Experience</li>
-                <li>1 - Fundamental Awareness</li>
-                <li>2 - Novice</li>
-                <li>3 - Intermediate</li>
-                <li>4 - Advanced</li>
+                <li>1 - Beginner</li>
+                <li>2 - Average</li>
+                <li>3 - Skilled</li>
+                <li>4 - Specialist</li>
                 <li>5 - Expert</li>
             </ul>
-            <div data-aos="fade-up"><CanvasJSReact.CanvasJSChart options = {options}/>{}</div>
+            <div className="chart" data-aos="fade-up"><CanvasJSReact.CanvasJSChart options = {techSkills}/>{}</div>
+            <div className="chart" data-aos="fade-up"><CanvasJSReact.CanvasJSChart options = {softSkills}/>{}</div>
         </section>
 	);
 }
