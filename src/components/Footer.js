b6fbb360a122ce2880654522f0b72c5ba158ede1
Jay Bisa <jay.bisa@gmail.com>
2020-12-30 11:52:15 -0500

    Use light github logo.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
         return(
             <footer className="footer py-3">
                 <div className="text-center text-muted">
-                    Designed & Built by Jay Bisa: <a href="https://github.com/jbisa/jaybisa-website" target="_blank" rel="noreferrer"><img src="github.png" alt="Github Logo"/></a>
+                    Designed & Built by Jay Bisa &nbsp;&nbsp;<a href="https://github.com/jbisa/jaybisa-website" target="_blank" rel="noreferrer"><img src="GitHub-Mark-Light-32px.png" alt="Github Logo"/></a>
                 </div>
             </footer>
         )
